df187e43391574bcec45f7f84d7430910f80a9c9
x94jp6rmp <x94jp6rmp@gmail.com>
2018-03-16 10:15:12 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+using namespace System;
+bool Guess(int number) {
+	static int target = -1;
+	if (target == -1) {
+		Random r;
+		target = r.Next() % 100 + 1;
+	}
+	if (number == target) {
+		std::cout << "Correct !!";
+		target = -1;
+		return true;
+	}
+	else std::cout << "Wrong" << std::endl;
+	return false;
+}
+int main(array<System::String ^> ^args)
+{
+	int guess;
+	do {
+		std::cin >> guess;
+	} while (!Guess(guess));
+	return 0;
+}
